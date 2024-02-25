f2f00b3dd673a99b70ac70503f2b575741faff77
Pedro Barbosa <pedro.barbosa@glintt.com>
2024-02-25 19:16:28 +0000

    v2 of advice generator: new color palette

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.scss";
 
 import Figure from "./components/molecules/Figure/Figure";
+import Heading from "./components/atoms/Heading/Heading";
 import React from "react";
 
 function App() {
   return (
     <main className="App advicegenerator flex flex--column flex__align--center">
       <Figure />
+      <Heading type="h2" text="Get inspired, pick your advice" className="mt--368" />
     </main>
   );
 }
